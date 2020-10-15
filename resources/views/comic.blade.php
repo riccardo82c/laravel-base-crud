ef664bd3e497621e11a88269655a63aeb537d3e9
Riccardo Colombo <rikyregna@gmail.com>
2020-10-15 16:36:26 +0200

    add bootstrap and nav

--- resources/views/comic.blade.php
+++ resources/views/comic.blade.php
@@ -4,7 +4,17 @@
 	 
 @section('main')
 
-	@dd($data);
-	 
+	@forelse ($data as $comic)
+	<ul>
+		<li>{{$comic->titolo}}</li>
+		<li>{{$comic->autore}}</li>
+		<li>{{$comic->prezzo}}</li>
+		<li>{{$comic->quantita}}</li>
+	</ul>
 
+	@empty
+
+	<p>Nessun record trovato!</p>
+		
+	@endforelse	
 @endsection
